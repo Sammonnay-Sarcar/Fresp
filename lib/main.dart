84cfa792b1ba94991e47eef3888b3f780c13944d
Sammonnay-Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2022-08-22 22:03:53 +0530

    patatri branch commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fresp',
-      theme: ThemeData(
-        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
-        colorScheme: const ColorScheme.light(
-          primary: GlobalVariables.secondaryColor,
+        title: 'Fresp',
+        theme: ThemeData(
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme: const ColorScheme.light(
+            primary: GlobalVariables.secondaryColor,
+          ),
+          appBarTheme: const AppBarTheme(
+              elevation: 0,
+              iconTheme: IconThemeData(
+                color: Colors.black,
+              )),
         ),
-        appBarTheme: const AppBarTheme(
-            elevation: 0,
-            iconTheme: IconThemeData(
-              color: Colors.black,
-            )),
-      ),
-      onGenerateRoute: (settings)=> generateRoute(settings),
-      home: const BottomBarScreen()
-    );
+        onGenerateRoute: (settings) => generateRoute(settings),
+        home: const BottomBarScreen());
   }
 }
 
